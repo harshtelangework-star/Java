988661474550531ea7dcac7d4251d6d8824f9b06
harshtelangework-star <harshtelangework@gmail.com>
2026-01-09 21:04:20 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,6 +1,15 @@
+//Declare two integer variables and:
+//Store any two numbers
+//Print their sum
+
 public class main{
-       public static void main(String[]args){
-       String name = "Pizza";
-       System.out.println("I Like"+' '+name);
-       }
+    public static void main(String[]args){
+        
+        int a = 10;
+        int b = 20;
+
+        int sum = a+b;
+
+        System.out.println(sum);
+    }
 }
